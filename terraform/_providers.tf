63e7a5c87c9c8897f61f7c275eeadf0d6ed615d8
Jacob Reynolds <jmreynolds03@gmail.com>
2025-01-02 18:01:59 +0000

    Updated _providers.tf

--- terraform/_providers.tf
+++ terraform/_providers.tf
@@ -10,6 +10,11 @@ terraform {
       storage_account_name = "jmreynolds03storage"
       container_name = "terraform-state"
       key = "terraform.tfstate"
+
+      client_id = "655ff4c2-7491-4ef3-847b-5cc89f032dc7"
+      client_secret = var.client_secret
+      tenant_id = "d2fc10a4-9803-4231-bdd0-9c75bfbde13b"
+      subscription_id = "0b6585a7-6c0e-4fe5-b3f2-a2e699a315e6"
     }
 }
 
