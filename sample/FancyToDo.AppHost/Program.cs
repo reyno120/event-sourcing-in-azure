bd4f83feed58f9104360c691c7357dd550b4ec78
00009775 <jacob.reynolds@jasperengines.com>
2025-02-03 12:39:49 -0500

    Attempting to add Azure Function to Aspire

--- sample/FancyToDo.AppHost/Program.cs
+++ sample/FancyToDo.AppHost/Program.cs
@@ -6,17 +6,22 @@ var useLocalEmulator = builder.AddParameter("UseLocalEmulator").Resource.Value.E
 // TODO: Can't get Azure function built from dockerfile in FunctionalTests to work with CosmosEmulator from linux container
 var cosmos = useLocalEmulator
     ? builder.AddConnectionString("fancy-cosmos")
-    : builder.AddAzureCosmosDB("fancy-cosmos").RunAsEmulator();
+    : builder.AddAzureCosmosDB("fancy-cosmos").RunAsEmulator(
+        emulator =>
+        {
+            emulator.WithLifetime(ContainerLifetime.Persistent);
+        });
 
-
+// AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;AccountEndpoint=https://127.0.0.1:60654;DisableServerCertificateValidation=True;
 var api = builder.AddProject<Projects.FancyToDo_API>("fancy-api")
     .WithReference(cosmos);
 
-// var funcApp = builder.AddAzureFunctionsProject<Projects.AzureFunctionsEndToEnd_Functions>("funcapp")
+// var funcApp = builder.AddAzureFunctionsProject<Projects.FancyToDo_Functions>("funcapp")
+//     .WithReference(cosmos)
+//     .WaitFor(api);
 
 if (!useLocalEmulator)
     api.WaitFor(cosmos);
-        // .WaitFor(funcApp);
 
 
 builder.Build().Run();
--- sample/FancyToDo.Functions/Program.cs
+++ sample/FancyToDo.Functions/Program.cs
@@ -3,7 +3,6 @@ using FancyToDo.Functions.Configuration;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,7 +16,6 @@ builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
-
 builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
 {
     var clientOptions = new CosmosClientOptions
@@ -25,19 +23,11 @@ builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
         SerializerOptions = new CosmosSerializationOptions
         {
             PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-        },
-        // WARNING
-        // TODO: Should only be used for local development
-        // https://learn.microsoft.com/en-us/azure/cosmos-db/how-to-develop-emulator?tabs=windows%2Ccsharp&pivots=api-nosql#import-the-emulators-tlsssl-certificate
-        HttpClientFactory = () => new HttpClient(new HttpClientHandler()
-        {
-            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-        }),
-        ConnectionMode = ConnectionMode.Gateway
+        }
     };
     
     return new CosmosClient(
-        builder.Configuration.GetConnectionString("CosmosDBConnectionString"), 
+        "fancy-cosmos",
         clientOptions
         );
 });
